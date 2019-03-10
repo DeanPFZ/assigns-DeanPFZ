c7008fd38633c83138ffdfba4371b8e5b2d280da
Pengfei Zhu <pengfei@royal-21.cs.wisc.edu>
2019-03-10 15:55:27 -0500

    Add top level workflow, missing part of alu and control logic.

--- project/demo1/verilog/rca_16b.v
+++ project/demo1/verilog/rca_16b.v
@@ -13,10 +13,10 @@ module rca_16b(A, B, C_in, S, C_out);
     input          C_in;
     output [N-1:0] S;
     output         C_out;
-	
+
 	wire[2:0] carry;
-	
-    rca_4b addBit0_3(.A(A[3:0]), .B(B[3:0]), .C_in(C_in), .S(S[3:0]), .C_out(carry[0]));
+
+  rca_4b addBit0_3(.A(A[3:0]), .B(B[3:0]), .C_in(C_in), .S(S[3:0]), .C_out(carry[0]));
 	rca_4b addBit4_7(.A(A[7:4]), .B(B[7:4]), .C_in(carry[0]), .S(S[7:4]), .C_out(carry[1]));
 	rca_4b addBit8_11(.A(A[11:8]), .B(B[11:8]), .C_in(carry[1]), .S(S[11:8]), .C_out(carry[2]));
 	rca_4b addBit12_15(.A(A[15:12]), .B(B[15:12]), .C_in(carry[2]), .S(S[15:12]), .C_out(C_out));
