b0760d0238e8a61ecbd774ac7e3127554a36a3be
Adam Czech <aczech2@wisc.edu>
2019-04-15 23:10:37 -0500

    Added direct cache; not working :(

---
+++ project/cache_direct/verilog/cache_fsm.v
@@ -0,0 +1,216 @@
+
+module cache_fsm(
+	//Output
+	fsm_done, cache_data_in, cache_comp, cache_enable, cache_wr, cache_valid_in, 
+	fsm_stall, fsm_err, cache_tag_in, cache_index, cache_offset, mem_addr, mem_wr,
+	mem_rd, mem_data_in, 
+	
+	// Input
+	DataIn_reg, cache_data_out, cache_hit, cache_dirty, cache_valid, Addr, Rd, Wr, clk, rst,
+	cache_tag_out, mem_DataOut
+	);
+
+   	localparam IDLE = 					4'b0000;
+   	localparam CHECK_HIT = 				4'b0001;
+   	localparam WRITE_BACK_MEM_WAIT =	4'b0010;
+   	localparam WRITE_BACK_MEM_WAIT_1 = 	4'b0011;
+   	localparam WRITE_BACK_MEM_WAIT_2 = 	4'b0100;
+   	localparam WRITE_BACK_MEM_WAIT_3 = 	4'b0101;
+   	localparam WRITE_BACK_MEM_WAIT_4 = 	4'b0110;
+   	localparam WRITE_BACK_MEM_WAIT_5 = 	4'b0111;
+   	localparam WRITE_BACK_MEM_WAIT_6 = 	4'b1000;
+   	localparam WRITE_BACK_MEM_DONE = 	4'b1001;
+   	localparam GET_MEM_DATA_1 = 		4'b1010;
+   	localparam GET_MEM_DATA_2 = 		4'b1011;
+   	localparam GET_MEM_DATA_3 = 		4'b1100;
+   	localparam GET_MEM_DATA_4 = 		4'b1101;
+   	localparam GET_MEM_DATA_5 = 		4'b1110;
+   	localparam REDO_READ = 				4'b1111;
+	
+	input [15:0] DataIn_reg, Addr, cache_data_out, mem_DataOut;
+	input cache_hit, cache_dirty, cache_valid, Rd, Wr, clk, rst;
+	input [4:0] cache_tag_out;
+
+	output reg fsm_done, cache_comp, cache_enable, cache_wr, cache_valid_in,
+				fsm_stall, fsm_err, mem_wr, mem_rd;
+	output reg [4:0] cache_tag_in;
+	output reg [7:0] cache_index;
+	output reg [2:0] cache_offset;
+	output reg [15:0] cache_data_in, mem_addr, mem_data_in;
+	
+   	wire [3:0] state;
+	reg [3:0] nxt_state;
+	reg4 reg_state(.q(state), .d(nxt_state), .clk(clk), .rst(rst));
+
+   	//assign DataOut = fsm_data_out;
+
+   	always @(*) begin
+      nxt_state = IDLE;
+      cache_enable = 1'b0;
+      cache_comp = 1'b0;
+      cache_wr = 1'b0;
+      cache_valid_in = 1'b1;
+      cache_data_in = DataIn_reg;
+      cache_tag_in = Addr[15:11];
+      cache_index = Addr[10:3];
+      cache_offset = Addr[2:0];
+      mem_addr = Addr;
+      mem_data_in = cache_data_out;
+      mem_wr = 1'b0;
+      mem_rd = 1'b0;
+      fsm_err = 1'b0;
+      fsm_stall = 1'b1;
+     //fsm_data_out = cache_data_out;
+      fsm_done = 1'b0;
+      case(state)
+     	IDLE: begin
+        	fsm_err = Rd&Wr;
+        	cache_enable = Rd^Wr;
+        	cache_comp = Rd^Wr;
+        	cache_wr = (~Rd)&Wr;
+			fsm_stall = 1'b0;
+        	nxt_state = (Rd^Wr)?CHECK_HIT:IDLE;
+      	end
+       	CHECK_HIT: begin
+        	fsm_done = cache_hit & cache_valid;
+        	mem_data_in = (~cache_hit & cache_valid & cache_dirty)?cache_data_out:DataIn_reg;
+        	mem_addr = (~cache_hit & cache_valid & cache_dirty)?{cache_tag_out,cache_index,3'b000}:
+						((cache_hit & ~cache_valid & Rd) | (~cache_hit & ~cache_dirty & Rd))?
+						{cache_tag_in,cache_index, 3'b000} : Addr;
+			cache_tag_in = cache_tag_out;
+			cache_offset = 3'b000;
+			cache_enable = 1'b1;
+			cache_wr = 1'b0;
+			fsm_stall = (cache_hit) ? 1'b0 : 1'b1;
+			mem_rd = ((cache_hit & ~cache_valid & Rd) | (~cache_hit & ~cache_dirty & Rd))? 1'b1 : 1'b0;
+			nxt_state = ((cache_hit & ~cache_valid & Rd) | (~cache_hit & ~cache_dirty & Rd))
+						? GET_MEM_DATA_1:
+						((~cache_hit & cache_valid & cache_dirty) ? WRITE_BACK_MEM_WAIT_1 : IDLE);
+       	end
+
+		//
+		// START OF MEM TO CACHE WRITE
+		//
+		// - Actually started in CHECK_HIT
+		// - Get 4 words of data from memory and write it to the cache
+		//
+		GET_MEM_DATA_1: begin
+			mem_rd = 1'b1;
+			mem_addr = {cache_tag_in, cache_index, 3'b010};
+			nxt_state = GET_MEM_DATA_2;
+		end
+		// This is the first cycle that memory data is available to write to the cache
+		GET_MEM_DATA_2: begin
+			mem_rd = 1'b1;
+			mem_addr = {cache_tag_in, cache_index, 3'b100};
+			cache_offset = 3'b000;
+			cache_enable = 1'b1;
+			cache_comp = 1'b0;
+			cache_wr = 1'b1;
+			cache_valid_in = 1'b1;
+			cache_data_in = mem_DataOut;
+			nxt_state = GET_MEM_DATA_3;
+		end
+		GET_MEM_DATA_3: begin
+			mem_rd = 1'b1;
+			mem_addr = {cache_tag_in, cache_index, 3'b110};
+			cache_offset = 3'b010;
+			cache_enable = 1'b1;
+			cache_comp = 1'b0;
+			cache_wr = 1'b1;
+			cache_valid_in = 1'b1;
+			cache_data_in = mem_DataOut;
+			nxt_state = GET_MEM_DATA_4;
+		end
+		GET_MEM_DATA_4: begin
+			cache_offset = 3'b100;
+			cache_enable = 1'b1;
+			cache_comp = 1'b0;
+			cache_wr = 1'b1;
+			cache_valid_in = 1'b1;
+			cache_data_in = mem_DataOut;
+			nxt_state = GET_MEM_DATA_5;
+		end
+		GET_MEM_DATA_5: begin
+			cache_offset = 3'b110;
+			cache_enable = 1'b1;
+			cache_comp = 1'b0;
+			cache_wr = 1'b1;
+			cache_valid_in = 1'b1;
+			cache_data_in = mem_DataOut;
+			nxt_state = GET_MEM_DATA_5;
+		end
+		// Now that the memory data is in the cache, redo the read instruction 
+		// This read should always hit
+		REDO_READ: begin
+			cache_enable = 1'b1;
+			cache_wr = 1'b0;
+			cache_comp = 1'b1;
+			nxt_state = CHECK_HIT;
+		end
+		//
+		// END OF MEMORY TO CACHE WRITE
+		//
+
+		//
+		// START CACHE TO MEM WRITE
+		//
+		// - Actually started in CHECK_HIT
+		// - Get 4 words of data from memory and write it to the cache
+		//
+		WRITE_BACK_MEM_WAIT: begin
+			cache_enable = 1'b1;
+			cache_wr = 1'b0;
+			cache_comp = 1'b0;
+			cache_offset = 3'b010;
+			mem_wr = 1'b1;
+        	mem_addr = {cache_tag_out,cache_index,3'b000};
+			nxt_state = WRITE_BACK_MEM_WAIT_1;
+		end
+		WRITE_BACK_MEM_WAIT_1: begin
+			cache_enable = 1'b1;
+			cache_wr = 1'b0;
+			cache_comp = 1'b0;
+			cache_offset = 3'b100;
+			mem_wr = 1'b1;
+        	mem_addr = {cache_tag_out,cache_index,3'b010};
+			nxt_state = WRITE_BACK_MEM_WAIT_2;
+		end
+		WRITE_BACK_MEM_WAIT_2: begin
+			cache_enable = 1'b1;
+			cache_wr = 1'b0;
+			cache_comp = 1'b0;
+			cache_offset = 3'b110;
+			mem_wr = 1'b1;
+        	mem_addr = {cache_tag_out,cache_index,3'b100};
+			nxt_state = WRITE_BACK_MEM_WAIT_3;
+		end
+		WRITE_BACK_MEM_WAIT_3: begin
+			cache_enable = 1'b1;
+			cache_wr = 1'b0;
+			cache_comp = 1'b0;
+			mem_wr = 1'b1;
+        	mem_addr = {cache_tag_out,cache_index,3'b110};
+			nxt_state = WRITE_BACK_MEM_WAIT_4;
+		end
+		WRITE_BACK_MEM_WAIT_4: begin
+			nxt_state = WRITE_BACK_MEM_WAIT_5;
+		end
+		WRITE_BACK_MEM_WAIT_5: begin
+			nxt_state = WRITE_BACK_MEM_WAIT_6;
+		end
+		// Last write (index = 2'b11) will finish the cycle after this state
+		// so we can start the memory to cache write now
+		WRITE_BACK_MEM_WAIT_6: begin
+        	mem_addr = {cache_tag_in,cache_index, 3'b000};
+			mem_rd = 1'b1;
+			nxt_state = GET_MEM_DATA_1;
+		end
+		//
+		// END CACHE TO MEM WRITE
+		//
+		// TODO add default state
+      endcase
+   end
+
+endmodule // cache_fsm
--- project/cache_direct/verilog/mem_system.v
+++ project/cache_direct/verilog/mem_system.v
@@ -27,26 +27,18 @@ module mem_system(/*AUTOARG*/
    wire [15:0] Addr_reg_in, DataIn_reg_in;
    wire Rd_reg, Wr_reg;
    wire Rd_reg_in, Wr_reg_in;
-   reg cache_data_in, cache_enable, cache_tag_in, cache_index
-       cache_offset, cache_comp, cache_wr, cache_valid_in; 
-   wire cache_dirty, cache_valid, cache_hit,  
-        cache_err, cache_data_out, cache_tag_out;
-   reg mem_data_in, mem_wr, mem_rd, mem_addr, 
-   wire mem_data_out, mem_stall, 
-        mem_busy, mem_err;
-   reg fsm_err, fsm_stall, fsm_data_out, fsm_done;
-   reg [3:0] state, nxt_state;
-   
-   localparam IDLE = 4'b0000;
-   localparam CHECK_HIT = 4'b0001;
-   localparam GET_MEM_DATA = 4'b0010;
-   localparam GET_MEM_WAIT = 4'b0011;
-   localparam MEM_DATA_READY = 4'b0100;
-   localparam WRITE_CACHE_WRITE = 4'b0101;
-   localparam WRITE_BACK_MEM_WAIT_1 = 4'b0110;
-   localparam WRITE_BACK_MEM_WAIT_2 = 4'b0111;
-   localparam WRITE_BACK_MEM_WAIT_3 = 4'b1000;
-   localparam WRITE_BACK_MEM_DONE = 4'b1001;
+   wire cache_enable, cache_comp, cache_wr, cache_valid_in; 
+   wire cache_dirty, cache_valid, cache_hit, cache_err;
+	wire [15:0] cache_data_out;
+	wire [15:0] cache_data_in;
+	wire [4:0] cache_tag_in;
+	wire [4:0] cache_tag_out;
+	wire [7:0] cache_index;
+	wire [2:0] cache_offset;
+	wire [15:0] mem_data_in, mem_data_out, mem_addr;
+   wire mem_wr, mem_rd, mem_stall, mem_err;
+	wire [3:0]  mem_busy;
+   wire fsm_err, fsm_stall, fsm_done;
    
    /* data_mem = 1, inst_mem = 0 *
     * needed for cache parameter */
@@ -85,54 +77,53 @@ module mem_system(/*AUTOARG*/
                      .wr                (mem_wr),
                      .rd                (mem_rd));
 
+	cache_fsm fsm(// Output
+					.fsm_done			(fsm_done),
+					.cache_data_in		(cache_data_in),
+					.cache_comp			(cache_comp),
+					.cache_enable		(cache_enable),
+					.cache_wr			(cache_wr),
+					.cache_valid_in		(cache_valid_in),
+					.fsm_stall			(fsm_stall),
+					.fsm_err			(fsm_err),
+					.cache_tag_in		(cache_tag_in),
+					.cache_index		(cache_index),
+					.cache_offset		(cache_offset),
+					.mem_addr			(mem_addr),
+					.mem_wr				(mem_wr),
+					.mem_rd				(mem_rd),
+					.mem_data_in		(mem_data_in),
+					// Input
+					.DataIn_reg			(DataIn_reg),
+					.cache_data_out		(cache_data_out),
+					.cache_hit			(cache_hit),
+					.cache_dirty		(cache_dirty),
+					.cache_valid		(cache_valid),
+					.Addr				(Addr_reg),
+					.Rd					(Rd_reg),
+					.Wr					(Wr_reg),
+					.clk				(clk),
+					.rst				(rst),
+					.cache_tag_out		(cache_tag_out),
+					.mem_DataOut		(mem_data_out));
+
+
    
-   // your code here
-   assign DataOut = fsm_data_out;
-   assign Done = fsm_done;
-   assign Stall = fsm_stall|mem_stall;
-   assign CacheHit = cache_hit;
-   assign err = (Addr_reg[0]==1'b1)|fsm_err|cache_err|mem_err;
-   
-   always begin
-      nxt_state = IDLE;
-	  cache_enable = 1'b0;
-	  cache_comp = 1'b0;
-	  cache_wr = 1'b0;
-	  cache_valid_in = 1'b1;
-	  cache_data_in = DataIn_reg;
-	  cache_tag_in = Addr_reg[15:11];
-	  cache_index = Addr_reg[10:3];
-	  cache_offset = Addr_reg[2:0];
-	  mem_addr = Addr_reg;
-	  mem_data_in = data_in;
-	  mem_wr = 1'b0;
-	  mem_rd = 1'b0;
-	  fsm_err = 1'b0;
-	  fsm_stall = 1'b0;
-	  fsm_data_out = cache_data_out;
-	  fsm_done = 1'b0;
-      case(state)
-	   IDLE: begin
-	    fsm_err = Rd&Wr;
-		cache_enable = Rd^Wr;
-		cache_comp = Rd^Wr;
-		cache_wr = ~Rd&Wr;
-		nxt_state = (Rd^Wr)?CHECK_HIT:IDLE;
-	   end
-	   CHECK_HIT: begin
-	    fsm_done = cache_hit & cache_valid;
-		mem_data_in = (~cache_hit & cache_valid & cache_dirty)?cache_data_out:data_in;
-		mem_wr = ~cache_hit & cache_valid & cache_dirty;
-		mem_addr = (~cache_hit & cache_valid & cache_dirty)?{cache_tag_out,cache_index,cache_offset}:Addr_reg;
-	   end
-	  endcase
-   end
+	assign DataOut = cache_data_out;
+	assign Done = fsm_done;	
+	assign CacheHit = cache_hit;
+	assign Stall = fsm_stall|mem_stall;
+    assign err = (Addr_reg[0]==1'b1)|fsm_err|cache_err|mem_err;
+
+	assign Addr_reg_in = (Stall) ? Addr_reg : Addr;
+	assign DataIn_reg_in = (Stall) ? DataIn_reg : DataIn;
+	assign Rd_reg_in = (Stall) ? Rd_reg : Rd;
+	assign Wr_reg_in = (Stall) ? Wr_reg : Wr;
   
    reg16 reg_Addr(.q(Addr_reg), .d(Addr_reg_in), .clk(clk), .rst(rst));
    reg16 reg_DataIn(.q(DataIn_reg), .d(DataIn_reg_in), .clk(clk), .rst(rst));
    dff reg_Rd(.q(Rd_reg), .d(Rd_reg_in), .clk(clk), .rst(rst));
    dff reg_Wr(.q(Wr_reg), .d(Wr_reg_in), .clk(clk), .rst(rst));
-   reg4 reg_state(.q(state), .d(nxt_state), .clk(clk), .rst(rst));
    
 endmodule // mem_system
 
