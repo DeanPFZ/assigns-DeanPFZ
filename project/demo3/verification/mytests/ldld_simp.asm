45342de222de14eb2645f776b2bde508ceaa94d1
Adam Czech <aczech2@wisc.edu>
2019-04-28 17:43:02 -0500

    Adding 3 load test to demo3

---
+++ project/demo3/verification/mytests/ldld_simp.asm
@@ -0,0 +1,19 @@
+lbi r1, 14
+ld r1, r1, 0
+ld r1, r1, 0
+ld r1, r1, 0
+//ld r1, r1, 0
+//addi r0, r0, -1
+//bgez r0, .loopstart
+halt
+halt
+data 0x000e
+data 0x000f
+data 0x0010
+data 0x0011
+data 0x0012
+data 0x0013
+data 0x0014
+data 0x0015
+data 0x0016
+data 0x0017
