d1ff57818c289cfc6a80de5e81b77eda38437a24
Adam Czech <aczech2@wisc.edu>
2019-04-29 19:46:47 -0500

    Added ld ld add test

---
+++ project/demo3/verification/mytests/ld_ld_add.asm
@@ -0,0 +1,23 @@
+//lbi r0, 0
+//lbi r1, 1
+//lbi r2, 2
+//lbi r3, 3
+//lbi r4, 4
+//lbi r5, 5
+//lbi r6, 6
+//ld r1, r0, 2
+//ld r3, r2, 2
+//ld r4, r6, 2
+
+
+ld r5, r2, 4
+ld r7, r0, 4
+add r7, r7, r5
+
+
+//add r7, r7, r4
+//add r7, r7, r3
+//add r7, r7, r1
+
+
+halt
