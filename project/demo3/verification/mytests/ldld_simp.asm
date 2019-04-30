a4240e92143d1866eb0d1b76d40f63bdd3f631a3
Adam Czech <aczech2@wisc.edu>
2019-04-30 11:29:15 -0500

    Fixed ldld_simp test and updated all.list for our tests

--- project/demo3/verification/mytests/ldld_simp.asm
+++ project/demo3/verification/mytests/ldld_simp.asm
@@ -1,19 +1,16 @@
 lbi r1, 14
 ld r1, r1, 0
-ld r1, r1, 0
-ld r1, r1, 0
 //ld r1, r1, 0
-//addi r0, r0, -1
-//bgez r0, .loopstart
+//ld r1, r1, 0
 halt
 halt
 data 0x000e
-data 0x000f
 data 0x0010
-data 0x0011
 data 0x0012
-data 0x0013
 data 0x0014
-data 0x0015
 data 0x0016
-data 0x0017
+data 0x0018
+data 0x001a
+data 0x001c
+data 0x001e
+data 0x0020
